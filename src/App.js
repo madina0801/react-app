7ca70b48c7d314014050fd70fc64aa5b1e8d8109
Madina <madinatussupova0820@gmail.com>
2022-05-26 11:40:03 +0600

    links and navigation added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetup";
 import FavouritesPage from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigationComponent";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupsPage />} />
