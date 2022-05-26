d1ed66b637315082f10ef7e21bf27346231ce540
Madina <madinatussupova0820@gmail.com>
2022-05-26 11:17:44 +0600

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
+import { Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
 
+function App() {
+  return <div>
+    <Route path='/' element={}></Route>
+    <Route path='/new-meetup'>
+      <NewMeetupsPage />
+    </Route>
+    <Route path="/favourites">
+      <FavouritesPage/>
+    </Route>
+  </div>;
 }
 
 export default App;
