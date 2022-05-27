0ebba04aa3a79cbdd98270e3366bae24aa2e53aa
Madina <madinatussupova0820@gmail.com>
2022-05-27 14:15:12 +0600

    get user input

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,24 +1,50 @@
+import { useRef } from 'react';
+
 import classes from './NewMeetupForm.module.css';
 import Card from '../ui/Card';
 
 function NewMeetupForm() {
+	const titleInputRef = useRef();
+	const imageInputRef = useRef();
+	const addressInputRef = useRef();
+	const descriptionInputRef = useRef();
+
+
+	function submitHandler(event) {
+		event.preventDefault();
+
+		const enteredTitle = titleInputRef.current.value;
+		const enteredImage = imageInputRef.current.value;
+		const enteredAddress = addressInputRef.current.value;
+		const enteredDescription = descriptionInputRef.current.value;
+
+		const meetupData = {
+			title: enteredTitle,
+			image: enteredImage,
+			address: enteredAddress,
+			description: enteredDescription
+		};
+
+		console.log(meetupData)
+	}
+
 	return <Card>
-		<form className={classes.form}>
+		<form className={classes.form} onSubmit={submitHandler}>
 			<div className={classes.control}>
 				<label htmlFor="title">Meetup Title</label>
-				<input type="text" id="title" required />
+				<input type="text" id="title" ref={titleInputRef} required />
 			</div>
 			<div className={classes.control}>
 				<label htmlFor="image">Meetup Image</label>
-				<input type="url" id="image" required />
+				<input type="url" id="image" ref={imageInputRef} required />
 			</div>
 			<div className={classes.control}>
 				<label htmlFor="address">Meetup Address</label>
-				<input type="text" id="address" required />
+				<input type="text" id="address" ref={addressInputRef} required />
 			</div>
 			<div className={classes.control}>
 				<label htmlFor="description">Meetup Description</label>
-				<textarea id="description required" rows="5"></textarea>
+				<textarea id="description required" ref={descriptionInputRef} rows="5"></textarea>
 			</div>
 			<div className={classes.actions}>
 				<button>Add Meetup</button>
